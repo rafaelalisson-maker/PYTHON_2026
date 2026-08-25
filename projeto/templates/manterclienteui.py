8badb8bb4f54ff07e40f7097ee1228dab0182dea
rafaelalisson-maker <rafael.alisson@escolar.ifrn.edu.br>
2026-08-25 18:34:33 +0000

    tarefa09

---
+++ projeto/templates/manterclienteui.py
@@ -0,0 +1,11 @@
+from templates.manterclienteui import manterclienteui
+from templates.manterservicoui import manterservicoui
+import streamlit as st
+
+class indexUI:
+    def main():
+        op = sidebar.slectbox("meno, ["clientes, Serviços]")
+        if op == "clientes": manterclienteui.main()
+        if op == "serviços": manterservicoui.main()
+
+indexUI.main()
